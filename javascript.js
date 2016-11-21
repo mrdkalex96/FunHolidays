d8bfd46aa6fa5ff68e0c01fde9c8be744066e84d
mrdkalex96 <ale.cara96@gmail.com>
2016-11-21 14:23:06 +0000

    update 1.1
    validation through modification of html elements

--- javascript.js
+++ javascript.js
@@ -1,67 +1,69 @@
 // function to validate the form
-function validateForm(){
+
+function validateForm() {
 	// validate name
-	//var letters = /^[A-Za-z]+$/; 
-	var x = document.forms["onlineForm"]["name"].value;
-	//if (x.match(letters)){
-	//	return false;
-    //}
-    //else {
-    //	alert("Name must be alphabetical only")
-    //	return false;
-    //}
-	if (x == null || x == ""){
-		alert("Name must be filled out");
+	var letters = /^[a-zA-Z]+$/;
+	var numbers = /^[0-9]+$/;
+	x = document.forms["onlineForm"]["name"].value;
+	if (x == null || x == "") {
+		document.getElementById("errName").innerHTML = "Name is required";
+		return false;
+	}
+	if (x.match(letters)){
+		
+	}
+	else{
+		document.getElementById("errName").innerHTML = "Name must be alphabetical only";
 		return false;
 	}
 	// validate surname
 	y = document.forms["onlineForm"]["surname"].value;
-	if (y == null || y == ""){
-		alert("Surname must be filled out");
+	if (y == null || y == "") {
+		document.getElementById("errSurn").innerHTML = "Surname is required";
 		return false;
 	}
 	// validate contact phone number
 	z = document.forms["onlineForm"]["number"].value;
-	if (z == null || z == ""){
-		alert("Number must be filled out");
+	if (z == null || z == "") {
+		document.getElementById("errNum").innerHTML = "Number must be filled out";
 		return false;
 	}
-	if (isNaN(z)){
-		alert("Can only contain numbers");
+	if (isNaN(z)) {
+		document.getElementById("errNum").innerHTML = "Can only contain numbers";
 		return false;
 	}
 	// validate email
 	a = document.forms["onlineForm"]["email"].value;
-	if (a == null || a == ""){
-		alert("Email address must be filled out");
+	if (a == null || a == "") {
+		document.getElementById("errEmail").innerHTML = "Email address must be filled out";
 		return false;
 	}
 	// validate confirm email
 	b = document.forms["onlineForm"]["confirm_email"].value;
-	if (b != a){
-		alert("The email does not match");
+	if (b != a) {
+		document.getElementById("errConfEmail").innerHTML = "The two email do not match";
 		return false;
 	}
-	if (b == null || b == ""){
-		alert("You must confirm your email");
+	if (b == null || b == "") {
+		document.getElementById("errConEmail").innerHTML = "You must confirm your email";
 		return false;
 	}
 	// validate leaving from
 	c = document.forms["onlineForm"]["From"].value;
-	if (c == null || c == ""){
-		alert("Departure from must be filled out");
+	if (c == null || c == "") {
+		document.getElementById("errFrom").innerHTML = "Leaving from must be filled out";
 		return false;
 	}
 	// validate arriving to
 	d = document.forms["onlineForm"]["To"].value;
-	if (d == null || d == ""){
-		alert("Destination must be filled out");
+	if (d == null || d == "") {
+		document.getElementById("errTo").innerHTML = "Destination must be filled out";
 		return false;
 	}
 	// validate departure date
 	e = document.forms["onlineForm"]["Departure"].value;
-	if (d == null || d == ""){
-		alert("Departure date must be filled out");
+	if (e == null || e == "") {
+		document.getElementById("errDate").innerHTML = "Departure date must be filled out";
 		return false;
 	}
 	
